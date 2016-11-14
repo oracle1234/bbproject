c45a00247ecf274dd9d9fc91411bafc1a70aa693
user1 <user1@user1-PC>
2016-11-14 14:03:24 +0900

    erm

--- bbproject/src/main/java/dto/ThemeRecipeDTO.java
+++ bbproject/src/main/java/dto/ThemeRecipeDTO.java
@@ -2,8 +2,22 @@ package dto;
 
 public class ThemeRecipeDTO {
 	
+	// 레시피 번호
 	private int recipe_no;
+	// 테마 번호
+	private int theme_no;
 	
+	private String recipe_name;
+	
+	private String recipe_material;
+	
+	private String recipe_explaination;
+	
+	private String portion;
+	
+	private String time;
+	
+	private String difficulty; 
 	
 	
 }
