78eb7688ca60cb5edf2a357254317260c2fcfc62
user1 <user1@user1-PC>
2016-11-14 11:14:09 +0900

    11234

--- bbproject/oracledb/Project_ERM.sql
+++ bbproject/oracledb/Project_ERM.sql
@@ -1,61 +1,59 @@
 
 /* Drop Tables */
 
+DROP TABLE fb_request CASCADE CONSTRAINTS;
+DROP TABLE fb_basket CASCADE CONSTRAINTS;
 DROP TABLE fb_lately_product CASCADE CONSTRAINTS;
 DROP TABLE fb_coupon_book CASCADE CONSTRAINTS;
 DROP TABLE fb_coupon CASCADE CONSTRAINTS;
-DROP TABLE fb_request CASCADE CONSTRAINTS;
-DROP TABLE fb_basket CASCADE CONSTRAINTS;
 DROP TABLE fb_member CASCADE CONSTRAINTS;
 DROP TABLE fb_food_comment CASCADE CONSTRAINTS;
 DROP TABLE fb_foods CASCADE CONSTRAINTS;
 DROP TABLE fb_category CASCADE CONSTRAINTS;
 DROP TABLE fb_delivery_cost CASCADE CONSTRAINTS;
-DROP TABLE fb_theme_recipe CASCADE CONSTRAINTS;
+DROP TABLE fb_theme_receipe CASCADE CONSTRAINTS;
 DROP TABLE fb_theme CASCADE CONSTRAINTS;
 DROP TABLE fb_comment CASCADE CONSTRAINTS;
-DROP TABLE fb_board CASCADE CONSTRAINTS;
 DROP TABLE fb_qa_board CASCADE CONSTRAINTS;
+DROP TABLE fb_board CASCADE CONSTRAINTS;
 DROP TABLE fb_photo_board CASCADE CONSTRAINTS;
 
 
 
 /* Drop Sequences dfsddfggf*/
 
-DROP SEQUENCE SEQ_fb_coupon_coupon_no;
-DROP SEQUENCE SEQ_fb_coupon_book_couponbook_no;
-DROP SEQUENCE SEQ_fb_member_member_no;
-DROP SEQUENCE SEQ_fb_category_category_no;
-DROP SEQUENCE SEQ_fb_delivery_cost_fb_delivery_no;
-DROP SEQUENCE SEQ_fb_food_comment_fc_no;
-DROP SEQUENCE SEQ_fb_theme_theme_no;
-DROP SEQUENCE SEQ_fb_foods_foods_no;
-DROP SEQUENCE SEQ_fb_theme_receipe_receipe_no;
-DROP SEQUENCE SEQ_fb_comment_comment_no;
-DROP SEQUENCE SEQ_fb_qa_board_qa_no;
-DROP SEQUENCE SEQ_fb_board_board_no;
-DROP SEQUENCE SEQ_fb_photo_board_photo_no;
-DROP SEQUENCE SEQ_fb_theme_recipe_recipe_no;
+DROP SEQUENCE SEQ_coupon_no;
+DROP SEQUENCE SEQ_couponbook_no;
+DROP SEQUENCE SEQ_member_no;
+DROP SEQUENCE SEQ_category_no;
+DROP SEQUENCE SEQ_fb_delivery_no;
+DROP SEQUENCE SEQ_fc_no;
+DROP SEQUENCE SEQ_theme_no;
+DROP SEQUENCE SEQ_foods_no;
+DROP SEQUENCE SEQ_receipe_no;
+DROP SEQUENCE SEQ_comment_no;
+DROP SEQUENCE SEQ_qa_no;
+DROP SEQUENCE SEQ_board_no;
+DROP SEQUENCE SEQ_photo_no;
 
 
 
 
 /* Create Sequences */
 
-CREATE SEQUENCE SEQ_fb_coupon_coupon_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_coupon_book_couponbook_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_member_member_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_category_category_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_delivery_cost_fb_delivery_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_food_comment_fc_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_theme_theme_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_foods_foods_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_theme_receipe_receipe_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_comment_comment_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_qa_board_qa_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_board_board_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_photo_board_photo_no INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE SEQ_fb_theme_recipe_recipe_no INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE SEQ_coupon_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_couponbook_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_member_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_category_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_fb_delivery_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_fc_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_theme_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_foods_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_receipe_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_comment_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_qa_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_board_no INCREMENT BY 1 START WITH 1 nocache nocycle;
+CREATE SEQUENCE SEQ_photo_no INCREMENT BY 1 START WITH 1 nocache nocycle;
 
 
 
@@ -71,6 +69,7 @@ CREATE TABLE fb_request
 	delivery_condition varchar2(50) NOT NULL,
 	foods_no number NOT NULL
 );
+
 
 CREATE TABLE fb_basket
 (
@@ -88,23 +87,12 @@ CREATE TABLE fb_lately_product
 );
 
 
-
-
 CREATE TABLE fb_coupon
 (
 	coupon_no number NOT NULL,
 	coupon_name varchar2(50) NOT NULL,
 	coupon_discount number NOT NULL,
 	PRIMARY KEY (coupon_no)
-);
-
-
-CREATE TABLE fb_coupon_book
-(
-	couponbook_no number NOT NULL,
-	member_no number NOT NULL,
-	coupon_no number NOT NULL,
-	PRIMARY KEY (couponbook_no)
 );
 
 
@@ -123,15 +111,12 @@ CREATE TABLE fb_member
 );
 
 
-CREATE TABLE fb_request
+CREATE TABLE fb_coupon_book
 (
+	couponbook_no number NOT NULL,
 	member_no number NOT NULL,
-	price number NOT NULL,
-	amount number NOT NULL,
-	picture number NOT NULL,
-	day date NOT NULL,
-	delivery_condition varchar2(50) NOT NULL,
-	foods_no number NOT NULL
+	coupon_no number NOT NULL,
+	PRIMARY KEY (couponbook_no)
 );
 
 
@@ -143,36 +128,11 @@ CREATE TABLE fb_category
 );
 
 
-CREATE TABLE fb_basket
-(
-	member_no number NOT NULL,
-	foods_no number NOT NULL,
-	amount number NOT NULL
-);
-
-
 CREATE TABLE fb_delivery_cost
 (
 	fb_delivery_no number NOT NULL,
 	fb_delivery_cost number NOT NULL,
 	PRIMARY KEY (fb_delivery_no)
-);
-
-
-CREATE TABLE fb_foods
-(
-	foods_no number NOT NULL,
-	foods_name varchar2(50) NOT NULL,
-	price number NOT NULL,
-	weight varchar2(50) NOT NULL,
-	way varchar2(50) NOT NULL,
-	shelfLife varchar2(50) NOT NULL,
-	foods_explaination varchar2(2000) NOT NULL,
-	foods_material varchar2(2000) NOT NULL,
-	picture varchar2(500) NOT NULL,
-	category_no number NOT NULL,
-	fb_delivery_no number NOT NULL,
-	PRIMARY KEY (foods_no)
 );
 
 
@@ -195,30 +155,35 @@ CREATE TABLE fb_theme
 );
 
 
-CREATE TABLE fb_theme_recipe
+CREATE TABLE fb_foods
 (
-	recipe_no number NOT NULL,
+	foods_no number NOT NULL,
+	foods_name varchar2(50) NOT NULL,
+	price number NOT NULL,
+	weight varchar2(50) NOT NULL,
+	way varchar2(50) NOT NULL,
+	shelfLife varchar2(50) NOT NULL,
+	foods_explaination varchar2(2000) NOT NULL,
+	foods_material varchar2(2000) NOT NULL,
+	picture varchar2(500) NOT NULL,
+	category_no number NOT NULL,
+	fb_delivery_no number NOT NULL,
+	PRIMARY KEY (foods_no)
+);
+
+
+CREATE TABLE fb_theme_receipe
+(
+	receipe_no number NOT NULL,
 	theme_no number NOT NULL,
-	recipe_name varchar2(500) NOT NULL,
-	recipe_material varchar2(500) NOT NULL,
-	recipe_explaination varchar2(500) NOT NULL,
+	receipe_name varchar2(500) NOT NULL,
+	receipe_material varchar2(500) NOT NULL,
+	receipe_explaination varchar2(500) NOT NULL,
 	picture varchar2(500) NOT NULL,
 	portion varchar2(50) NOT NULL,
 	time varchar2(50) NOT NULL,
 	difficulty varchar2(10) NOT NULL,
-	PRIMARY KEY (recipe_no)
-);
-
-
-CREATE TABLE fb_board
-(
-	board_no number NOT NULL,
-	board_readcount number NOT NULL,
-	board_writer varchar2(100),
-	board_content varchar2(2000) NOT NULL,
-	board_reg_date date NOT NULL,
-	board_subject varchar2(200) NOT NULL,
-	PRIMARY KEY (board_no)
+	PRIMARY KEY (receipe_no)
 );
 
 
@@ -246,8 +211,19 @@ CREATE TABLE fb_qa_board
 	qa_content varchar2(2000) NOT NULL,
 	qa_reg_date date NOT NULL,
 	qa_upload varchar2(500) NOT NULL,
-	qa_subject varchar2(200) NOT NULL,
 	PRIMARY KEY (qa_no)
+);
+
+
+CREATE TABLE fb_board
+(
+	board_no number NOT NULL,
+	board_readcount number NOT NULL,
+	board_writer varchar2(100),
+	board_content varchar2(2000) NOT NULL,
+	board_reg_date date NOT NULL,
+	board_upload varchar2(500),
+	PRIMARY KEY (board_no)
 );
 
 
@@ -259,7 +235,6 @@ CREATE TABLE fb_photo_board
 	photo_content varchar2(2000) NOT NULL,
 	photo_reg_date date NOT NULL,
 	photo_upload varchar2(500) NOT NULL,
-	photo_subject varchar2(200) NOT NULL,
 	PRIMARY KEY (photo_no)
 );
 
@@ -273,7 +248,7 @@ ALTER TABLE fb_coupon_book
 ;
 
 
-ALTER TABLE fb_lately_product
+ALTER TABLE fb_basket
 	ADD FOREIGN KEY (member_no)
 	REFERENCES fb_member (member_no)
 ;
@@ -291,7 +266,7 @@ ALTER TABLE fb_request
 ;
 
 
-ALTER TABLE fb_basket
+ALTER TABLE fb_lately_product
 	ADD FOREIGN KEY (member_no)
 	REFERENCES fb_member (member_no)
 ;
@@ -309,13 +284,13 @@ ALTER TABLE fb_foods
 ;
 
 
-ALTER TABLE fb_lately_product
-	ADD FOREIGN KEY (foods_no)
-	REFERENCES fb_foods (foods_no)
+ALTER TABLE fb_theme_receipe
+	ADD FOREIGN KEY (theme_no)
+	REFERENCES fb_theme (theme_no)
 ;
 
 
-ALTER TABLE fb_food_comment
+ALTER TABLE fb_lately_product
 	ADD FOREIGN KEY (foods_no)
 	REFERENCES fb_foods (foods_no)
 ;
@@ -327,21 +302,21 @@ ALTER TABLE fb_basket
 ;
 
 
-ALTER TABLE fb_theme_recipe
-	ADD FOREIGN KEY (theme_no)
-	REFERENCES fb_theme (theme_no)
-;
-
-
-ALTER TABLE fb_comment
-	ADD FOREIGN KEY (board_no)
-	REFERENCES fb_board (board_no)
+ALTER TABLE fb_food_comment
+	ADD FOREIGN KEY (foods_no)
+	REFERENCES fb_foods (foods_no)
 ;
 
 
 ALTER TABLE fb_comment
 	ADD FOREIGN KEY (qa_no)
 	REFERENCES fb_qa_board (qa_no)
+;
+
+
+ALTER TABLE fb_comment
+	ADD FOREIGN KEY (board_no)
+	REFERENCES fb_board (board_no)
 ;
 
 
