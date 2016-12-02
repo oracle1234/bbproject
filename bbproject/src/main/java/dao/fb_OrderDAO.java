a631b865e62582c91a818d92ed7538d04da9c79b
user1 <user1@user1-PC>
2016-12-02 17:05:28 +0900

    1202

--- bbproject/src/main/java/dao/fb_OrderDAO.java
+++ bbproject/src/main/java/dao/fb_OrderDAO.java
@@ -8,8 +8,7 @@ import dto.fb_OrderDTO;
 public interface fb_OrderDAO {
 	
 	public List<fb_OrderDTO> list(int num);
-	//public fb_OrderDTO delete(int member_no, int foods_no, Date day);
-	//List<fb_OrderDTO> delete(String member_no, String foods_no, String day);
+	public List<fb_OrderDTO> orderdelete(String member_no, String foods_no, String day);
 
 
 }
